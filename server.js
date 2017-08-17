a21a9f2346a510317743b31ab102f9a03a4ac51b
Jyothsna P <jyothsnap97@gmail.com>
2017-08-17 19:14:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ var articleOne = {
   
 };
 function createTemplate (data) {
-var title=data.date;
-var heading=data.heading;
-var content=data.content;
-var htmlTemplate=
-`<html>
+var title = data.title;
+var heading = data.heading;
+var date = data.date; 
+var content = data.content;
+var htmlTemplate =`
+<html>
 <head>
     <title>
         ${title}
@@ -58,7 +59,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req, res){
+app.get('/article-one',function(req, res) {
     res.send(createTemplate(articleOne));
 });
 
