d51185cd6547d8c2c9b5edb2ffb71dde9f7f159b
Jyothsna P <jyothsnap97@gmail.com>
2017-08-22 21:29:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,15 @@
-console.log('Loaded!');
-var img= document.getElementById("madi");
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 3;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function ()
-{ 
-  var interval = setInterval(moveRight, 50);  
+var button = document.getElementById("counter");
+button.onclick = function() {
+    var request= new XMLHttpRequest();
+    request.readystatechange = function() { 
+        if (request.readystate === XMLHttpRequest.DONE)
+        { if (request.status===200)
+           { var counter = request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML = counter.toString();
+           }
+        }
+    };
+    request.open('GET','http://jyothsnap97.imad.hasura-app.io/counter', true);
+    request.send(NULL);
 };
