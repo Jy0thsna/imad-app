f3f2dae57832e1b496403c8edf77904670387cb8
Jyothsna P <jyothsnap97@gmail.com>
2017-09-10 09:05:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,15 +71,6 @@ app.get('/ui/style.css', function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/articles', function(req, res) {
-    pool.query('SELECT * FROM article', function(err, result) {
-        if(err) {
-            res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result.rows[0]));
-        }
-  });
-});
 app.get('/:articleName',function(req, res) {
     
     pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName] , function (err, result) {
@@ -89,7 +80,7 @@ app.get('/:articleName',function(req, res) {
             if (result.rows.length === 0) {
                 res.status(404).send("Article Not Found");
             } else {
-                var articleData = result.rows[0];
+                var articleData = result.rows;
                 res.send(createTemplate(articleData));
             }
         }
