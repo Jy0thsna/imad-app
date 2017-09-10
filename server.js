0c413143f6124b155e43047143329281689cdd52
Jyothsna P <jyothsnap97@gmail.com>
2017-09-10 09:06:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/articles/:articleName',function(req, res) {
             if (result.rows.length === 0) {
                 res.status(404).send("Article Not Found");
             } else {
-                var articleData = result.rows;
+                var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
             }
         }
